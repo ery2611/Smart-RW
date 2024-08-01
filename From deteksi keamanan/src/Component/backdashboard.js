882073c88ca2f9d2f9919f87bb9f38bb1f148b1e
kautsarhakam <kautsarhakam16@gmail.com>
2024-08-01 15:36:20 +0700

    From Deteksi Keamanan

---
+++ From deteksi keamanan/src/Component/backdashboard.js
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import { useState } from 'react';
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import Link from '@mui/material/Link';
+
+function handleClick(event) {
+  event.preventDefault();
+  console.info('You clicked a breadcrumb.');
+}
+
+export default function BackDashboard() {
+  const [selected, setSelected] = useState('');
+
+  const handleLinkClick = (event, linkName) => {
+    event.preventDefault();
+    setSelected(linkName);
+    console.info(`You clicked the ${linkName} link.`);
+  };
+
+  return (
+    <div role="presentation">
+      <Breadcrumbs aria-label="breadcrumb">
+        <Link
+          underline="hover"
+          color="inherit"
+          href="/"
+          onClick={(event) => handleLinkClick(event, 'Dashboard')}
+          style={{ fontWeight: selected === 'Dashboard' ? 'bold' : 'normal' }}
+        >
+          Dashboard
+        </Link>
+        <Link
+          underline="hover"
+          color="inherit"
+          href="/material-ui/getting-started/installation/"
+          onClick={(event) => handleLinkClick(event, 'One Gate System')}
+          style={{ fontWeight: selected === 'One Gate System' ? 'bold' : 'normal' }}
+        >
+          One Gate System
+        </Link>
+        <Link
+          underline="hover"
+          color="inherit"
+          href="/material-ui/getting-started/installation/"
+          onClick={(event) => handleLinkClick(event, 'Notifikasi')}
+          style={{ fontWeight: selected === 'Notifikasi' ? 'bold' : 'normal' }}
+        >
+          From OGS
+        </Link>
+      </Breadcrumbs>
+    </div>
+  );
+}
