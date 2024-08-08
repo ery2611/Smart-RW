8ef521ecfb9d9ab3d28e7106d910242cd0e40d98
kautsarhakam <kautsarhakam16@gmail.com>
2024-08-08 15:09:51 +0700

    memunculkan button panggil

--- dashboardpanggilan/panggilanpickpetugas.js
+++ dashboardpanggilan/panggilanpickpetugas.js
@@ -1,4 +1,3 @@
-//EWS
 import React, { useState } from "react";
 import {
   Paper,
@@ -15,53 +14,49 @@ import {
   Typography,
   Chip,
   Pagination,
+  Checkbox,
+  Box,
 } from "@mui/material";
-import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
 import SearchIcon from "@mui/icons-material/Search";
-import AddIcon from "@mui/icons-material/Add";
-import Box from '@mui/material/Box';
-import ControlledCheckbox from "./Component/checkbox";
-
 
 const data = [
-  // Your data array
   {
-    id: <ControlledCheckbox/>,
+    id: 1,
     judul_bencana: "Yuni",
     jenis_bencana: "Petugas Keamanan",
     lokasi: "Area RW02 Kemang Pratama",
     peringatan: "AKTIF",
   },
   {
-    id: <ControlledCheckbox/>,
+    id: 2,
     judul_bencana: "Firdaus",
     jenis_bencana: "Petugas Keamanan",
     lokasi: "Kemang Dahlia Raya Blok Z No.12",
     peringatan: "TIDAK AKTIF",
   },
   {
-    id: <ControlledCheckbox/>,
+    id: 3,
     judul_bencana: "Yusuf",
     jenis_bencana: "Petugas Keamanan",
     lokasi: "Area RW02 Kemang Pratama",
     peringatan: "AKTIF",
   },
   {
-    id: <ControlledCheckbox/>,
+    id: 4,
     judul_bencana: "Wasis Utomo",
     jenis_bencana: "Petugas Keamanan",
     lokasi: "Area RW02 Kemang Pratama",
     peringatan: "TIDAK AKTIF",
   },
   {
-    id: <ControlledCheckbox/>,
+    id: 5,
     judul_bencana: "Bintang",
     jenis_bencana: "Petugas Keamanan",
     lokasi: "Area RW02 Kemang Pratama",
     peringatan: "AKTIF",
   },
   {
-    id: <ControlledCheckbox/>,
+    id: 6,
     judul_bencana: "Hakam",
     jenis_bencana: "Petugas Keamanan",
     lokasi: "Area RW02 Kemang Pratama",
@@ -73,12 +68,10 @@ const getPeringatanColor = (peringatan) => {
   switch (peringatan) {
     case "AKTIF":
       return {
-
         backgroundColor: "#DBF8DC",
         color: "#5AF411",
         fontWeight: "bold",
       };
-  
     case "TIDAK AKTIF":
       return {
         backgroundColor: "#FBE3E3",
@@ -90,10 +83,10 @@ const getPeringatanColor = (peringatan) => {
   }
 };
 
-
 const Pilihpetugas = () => {
   const [searchText, setSearchText] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
+  const [selectedCheckboxes, setSelectedCheckboxes] = useState([]);
   const itemsPerPage = 5;
 
   const handleSearchChange = (event) => {
@@ -101,7 +94,7 @@ const Pilihpetugas = () => {
   };
 
   const handleSearch = () => {
-    // Lakukan pencarian berdasarkan selectedDate dan searchText
+    // Lakukan pencarian berdasarkan searchText
   };
 
   const handleAdd = () => {
@@ -110,6 +103,14 @@ const Pilihpetugas = () => {
 
   const handlePageChange = (event, value) => {
     setCurrentPage(value);
+  };
+
+  const handleCheckboxChange = (id) => {
+    setSelectedCheckboxes((prevSelected) =>
+      prevSelected.includes(id)
+        ? prevSelected.filter((item) => item !== id)
+        : [...prevSelected, id]
+    );
   };
 
   // Get current data
@@ -131,30 +132,24 @@ const Pilihpetugas = () => {
           <Typography variant="h8" sx={{ color: "#A0A1A4" }}>
             Dashboard/
           </Typography>
-          
         </Stack>
 
         <Typography variant="h6" sx={{ textAlign: 'center' }}>
-              PILIH PETUGAS
-            </Typography>
+          PILIH PETUGAS
+        </Typography>
         <Box>
-      <Stack direction="row" spacing={5} sx={{ml: 5 ,mt: 4,}}>
-        <Typography sx={{ color: '#A1A5B7', fontSize: '16px', textAlign: 'left' }}>Panggilan Yang Dipilih</Typography>
-        <Typography sx={{ color: '#000', fontSize: '16px', textAlign: 'left', }}>: Keamanan</Typography>
-      </Stack>
-    </Box>
-          
-
-       
+          <Stack direction="row" spacing={5} sx={{ ml: 5, mt: 4 }}>
+            <Typography sx={{ color: '#A1A5B7', fontSize: '16px', textAlign: 'left' }}>Panggilan Yang Dipilih</Typography>
+            <Typography sx={{ color: '#000', fontSize: '16px', textAlign: 'left' }}>: Keamanan</Typography>
+          </Stack>
+        </Box>
       </Stack>
       <Stack
         direction="row"
         spacing={5}
         alignItems="center"
-        sx={{ marginBottom: 5,ml : 5, }}
+        sx={{ marginBottom: 5, ml: 5 }}
       >
-      
-
         <TextField
           value={searchText}
           onChange={handleSearchChange}
@@ -163,90 +158,71 @@ const Pilihpetugas = () => {
           InputProps={{
             startAdornment: <SearchIcon />,
           }}
-          sx={{ width: 250, height: 22, }}
+          sx={{ width: 250, height: 22 }}
         />
-         <Button
-  variant="contained"
-  onClick={handleSearch}
-  sx={{ backgroundColor: "#00A9AD", height: 38, mt: '20px !important' }}
->
-  CARI
-</Button>
-
-     
         <Button
           variant="contained"
-          color="primary"
-          onClick={handleAdd}
-          style={{ marginLeft: "auto",marginTop: 'auto', }}
-          sx={{
-            backgroundColor: "#00A9AD",
-            
-            height: 38,
-            display: "flex",
-            alignItems: "center",
-            mr : '25px !important',
-          }}
+          onClick={handleSearch}
+          sx={{ backgroundColor: "#00A9AD", height: 38, mt: '20px !important' }}
         >
-          PANGGIL
+          CARI
         </Button>
+
+        <Stack direction="row" justifyContent="end" spacing={2} sx={{width:"100%"}}>
+          {selectedCheckboxes.length > 0 && (
+            <Typography sx={{ fontSize: 18 }}>
+              {selectedCheckboxes.length} petugas
+            </Typography>
+
+          )}
+          {selectedCheckboxes.length > 0 && (
+            <Button
+            size="small"
+              variant="contained"
+              color="primary"
+              onClick={handleAdd}
+              sx={{
+                backgroundColor: "#00A9AD",
+                height: 38,
+                display: "flex",
+                alignItems: "center",
+                mr: '25px !important',
+                ml: "auto",
+              }}
+            >
+              PANGGIL
+            </Button>
+          )}
+        </Stack>
       </Stack>
       <TableContainer component={Paper} sx={{ flexGrow: 1 }}>
         <Table>
           <TableHead>
             <TableRow>
-              <TableCell
-                sx={{
-                  fontFamily: "Montserrat, sans-serif",
-                  fontWeight: "bold",
-                  color: "#A0A1A4",
-                }}
-              >
+              <TableCell sx={{ fontFamily: "Montserrat, sans-serif", fontWeight: "bold", color: "#A0A1A4" }}>
                 NO
               </TableCell>
-              <TableCell
-                sx={{
-                  fontFamily: "Montserrat, sans-serif",
-                  fontWeight: "bold",
-                  color: "#A0A1A4",
-                }}
-              >
+              <TableCell sx={{ fontFamily: "Montserrat, sans-serif", fontWeight: "bold", color: "#A0A1A4" }}>
                 BENCANA
               </TableCell>
-              
-             
-              <TableCell
-                sx={{
-                  fontFamily: "Montserrat, sans-serif",
-                  fontWeight: "bold",
-                  color: "#A0A1A4",
-                }}
-              >
+              <TableCell sx={{ fontFamily: "Montserrat, sans-serif", fontWeight: "bold", color: "#A0A1A4" }}>
                 LOKASI
               </TableCell>
-              <TableCell
-                sx={{
-                  fontFamily: "Montserrat, sans-serif",
-                  fontWeight: "bold",
-                  color: "#A0A1A4",
-                }}
-              >
+              <TableCell sx={{ fontFamily: "Montserrat, sans-serif", fontWeight: "bold", color: "#A0A1A4" }}>
                 STATUS
               </TableCell>
-              
+              <TableCell />
             </TableRow>
           </TableHead>
           <TableBody>
             {currentData.map((row) => (
-              <TableRow
-                key={row.id}
-                sx={{
-                  marginBottom: 2,
-                  display: "table-row",
-                  borderBottom: "none",
-                }}
-              >
-                <TableCell>{row.id}</TableCell>
+              <TableRow key={row.id}>
+                <TableCell>
+                  <Checkbox
+                    checked={selectedCheckboxes.includes(row.id)}
+                    onChange={() => handleCheckboxChange(row.id)}
+                  />
+                </TableCell>
                 <TableCell>
                   <Stack direction="column" alignItems="left">
                     <span>{row.judul_bencana}</span>
@@ -263,9 +239,7 @@ const Pilihpetugas = () => {
                   />
                 </TableCell>
                 <TableCell>
-                  <IconButton aria-label="Example">
-                   
-                  </IconButton>
+                  <IconButton aria-label="Example"></IconButton>
                 </TableCell>
               </TableRow>
             ))}
@@ -279,8 +253,8 @@ const Pilihpetugas = () => {
           onChange={handlePageChange}
           sx={{
             "& .MuiPaginationItem-page.Mui-selected": {
-              backgroundColor: "#00A9AD", // Background color for selected page
-              color: "#fff", // Text color for selected page
+              backgroundColor: "#00A9AD",
+              color: "#fff",
             },
           }}
         />
