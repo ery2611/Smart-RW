e193e160ce2e2de8a0ee5811286c130308ba2f40
rafiifmz_ <rafifmfaiz@gmail.com>
2024-08-09 16:44:21 +0700

    Updates

--- src/routes/index.js
+++
@@ -1,8 +0,0 @@
-import mainRoutes from "./routes"
-import { useRoutes } from "react-router-dom"
-
-
-export default function Routes(){
-    const routing = useRoutes(mainRoutes)
-    return <>{routing}</>
-}
