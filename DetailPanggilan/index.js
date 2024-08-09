6410746c895a99e8f803b049f044caece535ba74
kautsarhakam <kautsarhakam16@gmail.com>
2024-08-09 15:41:48 +0700

    Detail Pangglan Darurat warga

---
+++ DetailPanggilan/index.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import Detaildatapanggilan from '../Component/detailpanggilan';
+import { Box, Container, Stack, Typography } from "@mui/material";
+
+function Detailpanggilandarurat() {
+  return (
+    <div>
+      <Stack direction="column">
+          <Typography
+            variant="h5"
+            sx={{
+              marginBottom: 0,
+              color: "#00A9AD",
+              fontSize: 18,
+              textAlign: "left",
+            }}
+          >
+            WARGA
+          </Typography>
+          <Stack direction="row" sx={{ marginBottom: 2, fontSize: 14 }}>
+            <Typography variant="h8" sx={{ color: "#A0A1A4" }}>
+            Dashboard/ 
+            </Typography>
+            <Typography
+              variant="h9"
+              sx={{ color: "black", fontWeight: "bold" }}
+            >
+              Detail Forum
+            </Typography>
+          </Stack>
+        </Stack>
+        <Box
+          sx={{
+            marginTop: "20px",
+            width: "100%", // memastikan Box mengisi lebar penuh container
+          }}
+        >
+         <Detaildatapanggilan/>
+        </Box>
+       
+        
+    </div>
+  )
+}
+
+export default Detailpanggilandarurat;
