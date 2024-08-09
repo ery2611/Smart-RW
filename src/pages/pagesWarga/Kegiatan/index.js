dbafb100d4206d4585b52e3f696bc366de2c7184
rafiifmz_ <rafifmfaiz@gmail.com>
2024-08-09 11:32:49 +0700

    update

--- src/pages/pagesWarga/Kegiatan/index.js
+++ src/pages/pagesWarga/Kegiatan/index.js
@@ -4,17 +4,6 @@ import Bar from "../../../components/WargaComponents/Navbar";
 import { Box, Stack, Typography, Card, CardContent } from "@mui/material";
 
 function index() {
-  const now = new Date();
-
-  // Format tanggal
-  const hari = now.toLocaleString("id-ID", { weekday: "long" }); // Nama hari
-  const tanggal = now.toLocaleString("id-ID", { day: "2-digit" }); // Tanggal
-  const bulan = now.toLocaleString("id-ID", { month: "2-digit" }); // Nama bulan
-  const tahun = now.getFullYear(); // Tahun
-
-  // Menyusun string tanggal
-  const formatHari = `${hari}`;
-  const formatTanggal = `${tanggal} / ${bulan} / ${tahun}`;
   return (
     <div
       sx={{
@@ -22,61 +11,34 @@ function index() {
       }}
     >
       <Bar />
-      
-      <Box
-          sx={{
-            height: "3%",
-          }}
-        >
-          <Stack direction="column">
-            <Typography
-              variant="h5"
-              sx={{ marginBottom: 0, color: "#00A9AD", fontSize: 18 }}
-            >
-              WARGA
-            </Typography>
-            <Stack direction="row" sx={{ marginBottom: 2, fontSize: 14 }}>
-              <Typography variant="h8" sx={{ color: "#A0A1A4" }}>
-                Dashboard/
-              </Typography>
-              <Typography
-                variant="h9"
-                sx={{ color: "black", fontWeight: "bold" }}
-              >
-                Kegiatan
-              </Typography>
-            </Stack>
-          </Stack>
-        </Box>
-      <Box sx={{justifyContent:'center',width:'100%', display:'flex' }}>
-          <Card sx={{ boxShadow: 5, maxHeight: "300px", width:'600px', borderRadius:'10px' }}>
-            <CardContent>
-              <Box sx={{ display: "fluid", justifyContent: "flex-start" }}>
-                <Box
-                  sx={{
-                    textAlign: "center",
-                  }}
-                >
-                  <Typography variant="h6" gutterBottom fontWeight="bold">
-                    INFORMASI HARI INI
-                  </Typography>
-                </Box>
-                <Box sx={{ display: "flex", justifyContent: "space-around" }}>
-                  <Box sx={{ display: "flex", justifyContent: "space-around" }}>
-                    <Typography sx={{color:'#A1A5B7'}}>Hari</Typography>
 
-                    <Typography>: {formatHari}</Typography>
-                  </Box>
-                  <Box sx={{ display: "flex", justifyContent: "space-around" }}>
-                    <Typography sx={{color:'#A1A5B7'}}>Tanggal</Typography>
-                    <Typography>: {formatTanggal}</Typography>
-                  </Box>
-                </Box>
-              </Box>
-            </CardContent>
-          </Card>
-        </Box>
-        <Box
+      <Box
+        sx={{
+          height: "3%",
+        }}
+      >
+        <Stack direction="column">
+          <Typography
+            variant="h5"
+            sx={{ marginBottom: 0, color: "#00A9AD", fontSize: 18 }}
+          >
+            WARGA
+          </Typography>
+          <Stack direction="row" sx={{ marginBottom: 2, fontSize: 14 }}>
+            <Typography variant="h8" sx={{ color: "#A0A1A4" }}>
+              Dashboard/
+            </Typography>
+            <Typography
+              variant="h9"
+              sx={{ color: "black", fontWeight: "bold" }}
+            >
+              Kegiatan
+            </Typography>
+          </Stack>
+        </Stack>
+      </Box>
+
+      <Box
         display="fluid"
         sx={{
           justifyContent: "flex-start",
